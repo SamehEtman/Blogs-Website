f5e347df5ce38b5f533f76f2e84bf7bf187b92a9
Sameh <samehothman20162017@gmail.com>
2021-11-09 00:33:09 +0200

    start the initial pages setup

---
+++ pages/posts/[slug].js
@@ -0,0 +1,5 @@
+const PostDetailPage = () => {
+  return <div>Post Deatil</div>;
+};
+
+export default PostDetailPage;
---
+++ pages/posts/index.js
@@ -0,0 +1,5 @@
+const AllPostsPage = () => {
+  return <div>All posts here</div>;
+};
+
+export default AllPostsPage;
