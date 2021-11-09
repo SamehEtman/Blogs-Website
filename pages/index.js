56927982bcd2dfc371acf35de3021e3c68f8f823
Sameh <samehothman20162017@gmail.com>
2021-11-09 13:09:42 +0200

    add local storage for posts

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import FeaturedPosts from '../components/home-page/featured-post';
+import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
-const DUMMYDATA = [
-  {
-    title: 'Sameh',
-    image: 'p.jpg',
-    excerpt: 'i love sameh',
-    date: '1998-10-11',
-    slug: 'sameh-is-gamed',
-  },
-  {
-    title: 'Sameh',
-    image: 'p.jpg',
-    excerpt: 'i love sameh',
-    date: '1998-10-11',
-    slug: 'sameh-is-gamed',
-  },{
-    title: 'Sameh',
-    image: 'p.jpg',
-    excerpt: 'i love sameh',
-    date: '1998-10-11',
-    slug: 'sameh-is-gamed',
-  },
-];
-const HomePage = () => {
+import { getFeaturedPosts } from '../lib/posts-util';
+
+const HomePage = (props) => {
   return (
     <React.Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMYDATA} />
+      <FeaturedPosts posts={props.posts} />
     </React.Fragment>
   );
 };
-
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 1800,
+  };
+}
 export default HomePage;
---
+++ posts/sameh-is-gamed.md
@@ -0,0 +1,8 @@
+---
+title: 'Sameh'
+image: 'p.jpg'
+excerpt: 'i love sameh'
+date: '1998-11-10'
+isFeatured: true
+---
+# Hello iam sameh
